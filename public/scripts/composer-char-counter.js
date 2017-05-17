9d28c4f438ee95bb31e79923a1a0f39d1de58eb5
Dimas Andiputra <andiputra.dimas@hotmail.com>
2017-05-17 00:00:30 +0000

    CSS styling for tweets done

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $(".new-tweet form").on("keyup", "textarea", function(){
+    var counter = $(this).parent().find(".counter");
+    var counterVal = 140 - this.value.length;
+    if ((counterVal) < 0) {
+      counter.text(counterVal);
+      counter.css('color', 'red');
+    } else {
+      counter.text(counterVal);
+      counter.css('color', 'black');
+    }
+  });
+});
+
+$(document).ready(function() {
+  $(".show-tweet").on("mouseover",".tweet", function(){
+    var fontAwe= $(this).find("footer").find(".links");
+    fontAwe.css("display", "inline");
+  });
+});
+
+$(document).ready(function() {
+  $(".show-tweet").on("mouseout",".tweet", function(){
+    var fontAwe= $(this).find("footer").find(".links");
+    fontAwe.css("display", "none");
+  });
+});
